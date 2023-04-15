828131afc6e1ab3c16eb91229bf49c695b5afcb0
WilliamLinZh <117697083+WilliamLinZh@users.noreply.github.com>
2023-04-15 20:03:00 +0800

    4 files modified

--- src/app/store/store.component.ts
+++ src/app/store/store.component.ts
@@ -1,8 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-store',
   templateUrl: './store.component.html',
   styleUrls: ['./store.component.css'],
 })
-export class StoreComponent {}
+export class StoreComponent implements OnInit {
+  constructor(private router: Router) {}
+  ngOnInit(): void {
+    this.router.navigate(['/']);
+  }
+}
